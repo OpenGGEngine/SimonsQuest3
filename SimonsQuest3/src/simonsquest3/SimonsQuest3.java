3a39fb061d2090621d1f6bd102b042b2b9bc052e
javster101 <javster101@outlook.com>
2017-05-27 11:45:15 -0400

    The First Commit

---
+++ SimonsQuest3/src/simonsquest3/SimonsQuest3.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simonsquest3;
+
+import com.opengg.core.engine.GGApplication;
+import com.opengg.core.engine.OpenGG;
+import com.opengg.core.render.window.WindowInfo;
+
+/**
+ *
+ * @author Javier
+ */
+public class SimonsQuest3 extends GGApplication{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        OpenGG.initialize(new SimonsQuest3(), new WindowInfo());
+    }
+
+    @Override
+    public void setup() {
+        
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void update() {
+       
+    }
+    
+}
